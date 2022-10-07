ec18c06b4eaeb7d104fea574888072e62c4876f5
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-07 13:32:34 -0700

    added clickable icons to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div>I am Header</div>;
+  return (
+    <header>
+      <div>
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://www.linkedin.com/in/anas-ryadi/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/anas-ryadi/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/anasryadi/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+
+      <div>
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </div>
+    </header>
+  );
 }
