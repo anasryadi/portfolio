8958067d7b4ba6f6ec8b4b6d296e2a9aa4831ff8
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-09 15:48:16 -0700

    added and styled text and picture in About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -5,11 +5,15 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div className="flex flex-col; relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 jutify evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col; relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
-
       <motion.img
         initial={{
           x: -200,
@@ -21,8 +25,24 @@ export default function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="https://i.ibb.co/Chs8q6C/IMG-8772.jpg"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounder-lg md:w-64 md:h-95 x;:2-[500px] xl:h-[600px]"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
-    </div>
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          background
+        </h4>
+        <p className="text-lg">
+          Software Engineer with a background in computer science and
+          information technology. Driven by tackling complex problems and
+          breaking them down into various elements to decipher the best solution
+          for improvement. Leveraging my background knowledge in computer
+          science to approach each project with a unique approach and develop
+          applications that keep the user in mind.
+        </p>
+      </div>
+    </motion.div>
   );
 }
