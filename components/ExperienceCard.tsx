6b6acf267f8818fbd5466a9d684c8fce639035c7
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-09 17:35:13 -0700

    added the work experience page and sample experience cards

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+export default function ExperienceCard({}: Props) {
+  return (
+    <article>
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        src="https://opportutoring.com/wp-content/uploads/2020/06/PzyG3jIX_400x400.jpg"
+        alt=""
+      />
+
+      <div className="ox-0 md:px-10">
+        <h4 className="text-4xl font-light">IT Specialist</h4>
+        <p className="font-bold text-2xl mt-1">Learnship Global English</p>
+        <div className="flex space-x-2 my-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src="https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png"
+            alt=""
+          />
+          <img
+            className="h-10 w-10 rounded-full"
+            src="https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png"
+            alt=""
+          />
+          <img
+            className="h-10 w-10 rounded-full"
+            src="https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png"
+            alt=""
+          />
+        </div>
+        <p className="uppercase py-5 text-gray-300">
+          Started work... - Ended...
+        </p>
+        <ul className="list-disc space-y-4 ml-5 text-lg">
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+        </ul>
+      </div>
+    </article>
+  );
+}
