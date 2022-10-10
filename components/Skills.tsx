03543b7d9e5f5e0b9d8114076590a0c00ace72ea
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-09 18:17:14 -0700

    added titles and styling to skills compoenent

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 const Skills = (props: Props) => {
   return (
-    <div>Skills</div>
-  )
-}
+    <motion.div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Skills
+      </h3>
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+        Hover over a skill for currency profieciency
+      </h3>
+      <div className="grid grid-cols-4 gap-5">
+        <Skill />
+      </div>
+    </motion.div>
+  );
+};
 
-export default Skills
+export default Skills;
