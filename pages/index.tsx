8b77283380af77451b151e0472e508ed6a8ae4f1
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-13 20:44:30 -0700

    replaces sample skill icons/percentage with skills rendered from sanity API

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const Home = ({pageInfo, experiences, projects, skills, socials}: Props) => {
       </section>
 
       <section id="skills" className="snap-start">
-        <Skills />
+        <Skills skills={skills} />
       </section>
 
       <section id="projects" className="snap-start">
