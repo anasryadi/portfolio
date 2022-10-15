9ea984d33fb8073f9a8cae46e15c85a6b1be9627
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-14 19:36:11 -0700

    added job title and company fetch to experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -24,8 +24,9 @@ export default function ExperienceCard({ experience }: Props) {
       />
 
       <div className="ox-0 md:px-10">
-        <h4 className="text-4xl font-light">IT Specialist</h4>
-        <p className="font-bold text-2xl mt-1">Learnship Global English</p>
+        {/* <h4 className="text-4xl font-light">IT Specialist</h4> */}
+        <h4 className="text-4xl font-light">{experience.jobTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{experience.company}</p>
         <div className="flex space-x-2 my-2">
           {experience.technologies.map((technology) => (
             <img
