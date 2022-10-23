a3710bac35b4dbf71e782ae4c698850f0e821916
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-22 23:30:10 -0700

    fixed getServerSideProps bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,7 +74,7 @@ const Home = ({pageInfo, experiences, projects, skills, socials}: Props) => {
 
 export default Home;
 
-export const getServerSideProps: GetStaticProps<Props> = async () => {
+export const getServerSideProps = async () => {
   const pageInfo: PageInfo = await fetchPageInfo();
   const experiences: Experience[] = await fetchExperiences();
   const skills: Skill[] = await fetchSkills();
